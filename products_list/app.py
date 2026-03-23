390e5bc3e00e8b561340307b43902e8efcf5d0ca
winnyineza <w.ineza@alustudent.com>
2026-03-23 13:29:49 +0200

    Prepare services for local and Code Engine deployment

--- products_list/app.py
+++ products_list/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 import json
+import os
 
 app = Flask("List of products")
 CORS(app)
@@ -28,6 +29,5 @@ def getDealers(product):
     return {"message":"Could not find dealers for this product"}
 
 if __name__=="__main__":
-    app.run(debug=True) 
-    # When no port is specified, starts at default port 5000
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
